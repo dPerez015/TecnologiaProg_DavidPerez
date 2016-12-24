cc56c21567fafeb82a3125c2f960beb53927ec65
dPerez015 <davidperezgallego@enti.cat>
2016-12-24 01:07:53 +0100

    Renderer, window y ids creadas, no acabadas.

--- src/Snake/GameEngine.h
+++ src/Snake/GameEngine.h
@@ -1,1 +1,26 @@
 #pragma once
+
+class GameEngine {
+	inline static GameEngine &Instance() {
+		static GameEngine gameEngine;
+		return gameEngine;
+	}
+	GameEngine() = default;//optimizacion
+	GameEngine(const GameEngine &rhs) = delete;//eliminamos la construccion por copia
+	GameEngine &operator=(const GameEngine &rhs) = delete;//eliminamos el operador "="
+
+	void loadRes(){
+		//Cargar los archivos en el renderer;
+	}
+
+public:
+	
+
+	void Run() {
+	
+	
+	}
+
+
+
+};
--- src/Snake/Renderer.h
+++ src/Snake/Renderer.h
@@ -1,2 +1,18 @@
 #pragma once
+#include <unordered_map>
+#include <SDL_image.h>
+#include "ID.h"
 
+class Renderer {
+	inline static Renderer &Instance() {
+		static Renderer rend;
+		return rend;
+	}
+	template <ObjectsID Id>void loadTexture(std::string filename) {
+		auto loadedTexture = IMG_Load(RESOURCE_FILE(filename));//es un define que copia "../../res/"+ filename y lo pasa a string
+	}
+
+private:
+	std::unordered_map<ObjectID, SDL_Texture> theImages;
+
+};
