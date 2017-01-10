425491a1b54357aa143b5723aae3adf70f29a3eb
dPerez015 <davidperezgallego@enti.cat>
2017-01-10 11:11:12 +0100

    Algo del Hud hecho

--- src/Snake/Hud.h
+++ src/Snake/Hud.h
@@ -1,17 +1,29 @@
 #pragma once
 #include "Window.h"
 #include <SDL.h>
-class Hud
-{
+#include "Renderer.h"
+class Hud{
 public:
 	Hud() {
-	esquinas=
+		esquinasGrid = new SDL_Point[4];
+		esquinasGrid[0].x = W.GetWidth()*0.1/2;//limite horizontal izquierdo de la grid 
+		esquinasGrid[0].y = W.GetHeigth()*0.3 / 2;//limite vertical arriba
+		esquinasGrid[1].x = esquinasGrid[0].x;
+		esquinasGrid[1].y = W.GetHeigth() - (esquinasGrid[0].y);
+		esquinasGrid[2].x = W.GetWidth()-esquinasGrid[0].x;
+		esquinasGrid[2].y = esquinasGrid[1].y;
+		esquinasGrid[3].x = esquinasGrid[2].x;
+		esquinasGrid[3].y = esquinasGrid[0].y;
 	}
 	~Hud() {
 	
 	}
 
+	void draw() {
+		SDL_SetRenderDrawColor(R, 0, 0, 0, 255);
+	}
+
 private:
-	SDL_Point* esquinas;
+	SDL_Point* esquinasGrid;
 };
 
