2f0aab1c92d9e9ee7fbfda986cc46d3c55f13ba7
david lopez saludes <davidlopez1972@hotmail.es>
2017-01-30 14:02:31 +0100

    Binario acabado y  comentado

--- src/Snake/Ranking.h
+++ src/Snake/Ranking.h
@@ -6,79 +6,94 @@
 #include<vector>
 
 using std::ios;
-//Últim codi que hem vist, aquest és el del vector predeterminat
-namespace BinaryReadWrite {
 
-	struct PlayerScore
-	{
+namespace BinaryFiles {
+	//Struct de personas que guarda la persona y la puntuacion
+	struct HighScores {
 		std::string name;
 		int score;
-
 	};
+	//Array de 10 posiciones que servira para passar los jugadores al fichero y leerlo despues 
+	HighScores top10[10];
 
-	void sort(std::vector<PlayerScore> &rankToSort) {//Ordena el vector que se li passi
+	void sort() {//Funcion que ordena el array
 
-		PlayerScore tmp;
-
+		HighScores sortRank;
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
-				if (rankToSort[i].score > rankToSort[j].score) {
-					tmp = rankToSort[i];
-					rankToSort[i] = rankToSort[j];
-					rankToSort[j] = tmp;
+				if (top10[i].score > top10[j].score) {
+					sortRank = top10[i];
+					top10[i] = top10[j];
+					top10[j] = sortRank;
 				}
 			}
 		}
-
 	}
-
-	void writeBin(std::vector<PlayerScore> playerStats) {//Escriu el vector que se li passa en un fitxer binari
-		std::ofstream writeFile("../res/Ranking.bin", ios::binary);
-
+	//Funcion que ordena a un nuevo jugador en la posicion que le toca
+	void setNewRank(HighScores &newRank) {
 		for (int i = 0; i < 10; i++) {
-			writeFile.write(playerStats[i].name.c_str(), playerStats[i].name.size());
+			if (top10[i].score < newRank.score) {
+				for (int j = 10 - 1; j > i; j--) {
+					top10[j] = top10[j - 1];
+				}
+				top10[i] = newRank;
+				break;
+			}
+		}
+	}
+	//Funcion encargada de escribir el array en el fichero binario
+	void writeBin(std::string name, int score) {
+
+
+		std::ifstream readFile("../res/Ranking.bin", ios::binary);
+		//En este for leemos el fichero binario ya escrito y les pasamos el contenido de este al array
+		for (int i = 0; i < 10; i++) {
+			std::getline(readFile, top10[i].name, '\0');
+			readFile.read(reinterpret_cast<char*>(&top10[i].score), sizeof(top10[i].score));
+		}
+
+		readFile.close();
+		//Variable tipo struct que guarda los parametros de la funcion
+		HighScores toIns;
+		toIns.name = name;
+		toIns.score = score;
+
+		sort();//Llamada a la funcion que ordena el array
+		setNewRank(toIns);//Llamada a la funcion que pone al nuevo jugador en la posicion del array pertinente
+
+		std::ofstream writeFile("../res/Ranking.bin", ios::binary);
+		//Escribimos el array en el fichero binario
+		for (int i = 0; i < 10; i++) {
+			writeFile.write(top10[i].name.c_str(), top10[i].name.size());
 			writeFile.write("\0", sizeof(char));
-			writeFile.write(reinterpret_cast<char*>(&playerStats[i].score), sizeof(playerStats[i].score));
+			writeFile.write(reinterpret_cast<char*>(&top10[i].score), sizeof(top10[i].score));
 		}
 		writeFile.close();
-	}
 
-	std::vector<PlayerScore> readBin() {//Retorna un vector amb la info del fitxer binari
+	}
+	//Funcion que devuelbe un array con el contenido del fichero
+	HighScores readBin() {
 		std::ifstream readFile("../res/Ranking.bin", ios::binary);
 
-		std::vector<PlayerScore> ranking;
-
-		std::cout << "Players ranking: " << std::endl;
+		HighScores retRank[10];
 		for (int i = 0; i < 10; i++) {
-			std::getline(readFile, ranking[i].name, '\0');
-			readFile.read(reinterpret_cast<char*>(&ranking[i].score), sizeof(ranking[i].score));
+			std::getline(readFile, retRank[i].name, '\0');
+			readFile.read(reinterpret_cast<char*>(&retRank[i].score), sizeof(retRank[i].score));
 		}
 		readFile.close();
-		return ranking;
+		return retRank[10];
 	}
+	//Funcion que printa por consola el contenido del fichero(usada solo para comprobar que funciona)
+	void readBinCons() {
+		std::ifstream readFile("../res/Ranking.bin", ios::binary);
 
-
-	void mainFunct() {//Es crea un fitxer binari predeterminat de 10 pos que es passarà a les funcions anteriors
-
-		PlayerScore ply1 = { "Player", 0 };
-		PlayerScore ply2 = { "Player", 0 };
-		PlayerScore ply3 = { "Player", 0 };
-		PlayerScore ply4 = { "Player", 0 };
-		PlayerScore ply5 = { "Player", 0 };
-		PlayerScore ply6 = { "Player", 0 };
-		PlayerScore ply7 = { "Player", 0 };
-		PlayerScore ply8 = { "Player", 0 };
-		PlayerScore ply9 = { "Player", 0 };
-		PlayerScore ply10 = { "Player", 0 };
-
-		std::vector<PlayerScore> rankPlay = { ply1, ply2, ply3, ply4, ply5, ply6, ply7, ply8, ply9, ply10 };
-
-		if (rankPlay.size() > 10) {
-			rankPlay.pop_back();
+		HighScores rankPrint[10];
+		for (int i = 0; i < 10; i++) {
+			std::getline(readFile, rankPrint[i].name, '\0');
+			readFile.read(reinterpret_cast<char*>(&rankPrint[i].score), sizeof(rankPrint[i].score));
+			std::cout << i + 1 << "-Player name: " << rankPrint[i].name << "-Player score: " << rankPrint[i].score << std::endl;
 		}
-
-		sort(rankPlay);
-		writeBin(rankPlay);
-		readBin();
+		readFile.close();
 	}
+
 }
